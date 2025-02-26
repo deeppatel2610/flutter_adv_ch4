31e3433920ee8b0596a91fbf60d1c07a83f4a359
Administrator <deeppatel100267@gmail.com>
2025-02-26 09:59:44 +0530

    26 2 25

--- lib/in app web/web_get_controller.dart
+++ lib/in app web/web_get_controller.dart
@@ -1,23 +1,39 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_adv_ch4/in%20app%20web/db_helper.dart';
+import 'package:flutter_adv_ch4/in%20app%20web/web_model.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:get/get.dart';
 
-var getController = Get.put(WebGetController());
-
-class WebGetController extends GetxController {
-  var txtSearch = TextEditingController();
+class WebGetProvider extends ChangeNotifier {
   InAppWebViewController? webViewController;
-  RxInt listIndex = 0.obs;
+  var txtSearch = TextEditingController();
+  int indexValue = 0;
+  int valueHistory = 0;
+  List<WebModel> historyList = [];
 
-  List imageList = [
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png',
-    'https://w7.pngwing.com/pngs/423/261/png-transparent-bing-hd-logo-thumbnail.png',
-    'https://w7.pngwing.com/pngs/674/470/png-transparent-yandex-logo-y-search-engines-thumbnail.png',
-  ];
-
-  RxList linkList = [
+  List linkList = [
     "https://www.google.com/search?q=",
     "https://www.bing.com/search?q=",
     "https://yandex.com/search/?text=",
-  ].obs;
+    "https://search.yahoo.com/search?d=%7b%22dn%22%3a%22yk%22%2c%22subdn%22%3a%22software%22%2c%22ykid%22%3a%22e25d36d5-42c6-4ea8-bd32-02f1d387fde1%22%7d&fr2=p%3ads%2cv%3aomn%2cm%3asa%2cbrws%3achrome%2cpos%3a1&fr=mcafee&type=E210US885G0&p=",
+  ];
+
+  void chengValue({required int value}) {
+    valueHistory = value;
+    notifyListeners();
+  }
+
+  void chengIndex({required int index}) {
+    indexValue = index;
+    notifyListeners();
+  }
+
+  Future<void> readDataDb() async {
+    List data = await DbHelper.dbHelper.readData();
+    historyList = data
+        .map(
+          (e) => WebModel.fromMap(e),
+        )
+        .toList();
+    notifyListeners();
+  }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,8 +7,10 @@ import Foundation
 
 import connectivity_plus
 import flutter_inappwebview_macos
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
